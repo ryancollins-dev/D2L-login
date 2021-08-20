ec41b0ae4c1804ee366eed0936d5e93559037d6f
ryancollins-dev <info@ryancollins.dev>
2021-08-19 22:33:24 -0400

    Update main

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Program: D2L Login
+# Author: Ryan
+# Original creation date: 2021-08-19
+# Version: Final
+# Updated: 2021-08-19
+
 #!/usr/bin/env python3
 import time
 import yaml
@@ -8,28 +14,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 
+emailfield = (By.ID, "i0116")
+passwordfield = (By.ID, "i0118")
+nextbutton = (By.ID, "idSIButton9")
+no = (By.ID, "idBtn_Back")
+
 # login credentials
 credentials = yaml.load(open('./login.yml'), Loader=yaml.FullLoader)
 usernameStr = credentials['user']['email']
 passwordStr = credentials['user']['password']
 url = credentials['user']['url']
+
+# incognito add following argument
 # chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument("--incognito")
 browser = webdriver.Chrome('./chromedriver')
 browser.get(url)
 
 # Fill username and click
-username = browser.find_element_by_id('i0116')
-username.send_keys(usernameStr)
-button = browser.find_element_by_id('idSIButton9')
-browser.implicitly_wait(10)
-ActionChains(browser).move_to_element(button).click(button).perform()
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(emailfield)).send_keys(usernameStr)
+# username = browser.find_element_by_id('i0116')
+# username.send_keys(usernameStr)
+# button = browser.find_element_by_id('idSIButton9')
+# browser.implicitly_wait(10)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(nextbutton)).click()
 
 # Fill password and click
-password = WebDriverWait(browser, 5).until(
-    EC.presence_of_element_located((By.ID, 'i0118')))
-password.send_keys(passwordStr)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(passwordfield)).send_keys(passwordStr)
 
 # Sign in click
-signInButton = browser.find_element_by_id('idSIButton9')
-signInButton.click()
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(nextbutton)).click()
+
+# Click NO
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(no)).click()
